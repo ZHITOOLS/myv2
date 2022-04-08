a3c4c7bb1da31e544baf52dfac4dbbc216d846a3
ZHITOOLS <81851130+ZHITOOLS@users.noreply.github.com>
2022-04-08 10:12:53 +0800

    Add files via upload

--- Subconverter.vue
+++ Subconverter.vue
@@ -255,9 +255,9 @@ export default {
           "自动判断客户端": "auto",
         },
         customBackend: {
-          "loveyou": "https://sub.loveyou.eu.org/sub?",
+          "sub.loveyou.eu.org": "https://sub.loveyou.eu.org/sub?",
           "sub.xeton.dev(subconverter作者提供-稳定)":
-          "https://subcon.dlj.tf/sub?",
+          "https://sub.xeton.dev/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
           "api.wcc.best(sub-web作者提供-稳定)": "https://api.wcc.best/sub?",
           "sub.id9.cc(品云转换)": "https://sub.id9.cc/sub?",
